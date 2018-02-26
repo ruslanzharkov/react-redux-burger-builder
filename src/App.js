7e72180c9ae38c2bab7c3346cb28957af91688ac
rus <rus@mac.connectify>
2018-02-26 22:16:41 +0600

    Some changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello there!</h1>
+        <h1>Hi, I`m a React App</h1>
+        <p>This is really working</p>
       </div>
     );
   }
