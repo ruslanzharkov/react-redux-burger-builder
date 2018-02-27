d8f765edf0de051dd6a9009baa1cacf93fb0e05f
ruslanzharkov <rus_basket_kg@inbox.ru>
2018-02-27 20:12:35 +0600

    Burger hard code components

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+class Burger extends Component {
+    render() {
+        return (
+            <div className={classes.Burger}>
+                <BurgerIngredient type="bread-top"/>
+                <BurgerIngredient type="cheese"/>
+                <BurgerIngredient type="meat"/>
+                <BurgerIngredient type="bread-bottom"/>
+            </div>
+        );
+    };
+}
+export default Burger;
