91a6132d8fd22b75825da5703572a31dd3aa2651
ruslanzharkov <rus_basket_kg@inbox.ru>
2018-02-27 19:29:51 +0600

    Added full BurgerIngredient

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -17,12 +17,23 @@ const burgerIngredient = (props) => {
                 </div>
             );
             break;
-        case 'meat': 
-            
+        case 'meat':
+            ingredient = <div className={classes.Meat}></div>;
+            break;
+        case 'cheese':
+            ingredient = <div className={classes.Cheese}></div>;
+            break;
+        case 'bacon':
+            ingredient = <div className={classes.Bacon}></div>;
+            break;
+        case 'salad':
+            ingredient = <div className={classes.Salad}></div>;
             break;
         default:
+            ingredient = null;
             break;
     }
+    return ingredient;
 };
 
 export default burgerIngredient;
