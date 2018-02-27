9dd4e194c9a118f3f9f2029d34a599d95ccb0d25
ruslanzharkov <rus_basket_kg@inbox.ru>
2018-02-27 23:47:41 +0600

    Added build controls

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import classes from './BuildControl.css';
+
+class BuildControl extends Component {
+    render() {
+        return(
+            <div className={classes.BuildControl}>
+                <div className={classes.Label}>{this.props.label}</div>
+                <button className={classes.Less}>Less</button>
+                <button className={classes.More}>More</button>
+            </div>
+        );
+    }
+}
+
+export default BuildControl;
