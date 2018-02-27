b193f0c0a49950eb1a8872c9cd587da467e3456a
ruslanzharkov <rus_basket_kg@inbox.ru>
2018-02-27 23:19:36 +0600

    Added check for empty ingredients and handler for it

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -10,12 +10,14 @@ class Burger extends Component {
                 return [...Array(this.props.ingredients[ingredientKey])].map((_, i) => {
                     return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
                 });
-            });
+            }).reduce((arr, el) => {
+                return arr.concat(el);
+            }, []);
 
         return (
             <div className={classes.Burger}>
                 <BurgerIngredient type="bread-top"/>
-                {transformedIngredients}
+                { transformedIngredients.length === 0 ? <p>Please, start adding ingredients!</p> : transformedIngredients }
                 <BurgerIngredient type="bread-bottom"/>
             </div>
         );
