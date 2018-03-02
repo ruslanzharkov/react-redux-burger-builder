4d7aefd1903847b54bc83f59a02b73ab77ed9ac9
ruslanzharkov <rus_basket_kg@inbox.ru>
2018-03-02 21:59:20 +0600

    dynamicly button

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import classes from './Button.css';
+
+class Button extends Component {
+    render() {
+        return(
+            <button
+                onClick={this.props.clicked}
+                className={[classes.Button, classes[this.props.btnType].join(' ')]}>{this.props.children}</button>
+        );
+    }
+}
+
+export default Button;
