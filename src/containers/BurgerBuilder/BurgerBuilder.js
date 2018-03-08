5d47dc1b38740486d4cd0915371048970171a8de
ruslanzharkov <rus_basket_kg@inbox.ru>
2018-03-08 14:39:28 +0600

    Added post action tp firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICES = {
     salad: 0.5,
@@ -84,7 +85,23 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert('EEee boy');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Ruslan Zharkov',
+                address: {
+                    street: 'Teststreet1',
+                    zipCode: '41351',
+                    country: 'Russia'
+                },
+                email: 'test@test.com'
+            },
+            delieveryMethod: 'fastest'
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     };
 
     render() {
