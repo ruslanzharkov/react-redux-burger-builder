402cb4fc381d572dc2ca432e37dd10de1ac18b74
loki <rus_basket_kg@inbox.ru>
2018-03-15 00:08:38 +0600

    Added dynamicly view for checkout burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -129,7 +129,15 @@ class BurgerBuilder extends Component {
                 });
             });
 
-            this.props.history.push('/checkout');
+            const queryParams = [];    
+            for (let i in this.state.ingredients) {
+                queryParams.push(encodeURIComponent(i) + '=' + this.state.ingredients[i]);
+            }
+            const queryString = queryParams.join('&');
+            this.props.history.push({
+                pathname: '/checkout',
+                search: '?' + queryString
+            });
     };
 
     render() {
