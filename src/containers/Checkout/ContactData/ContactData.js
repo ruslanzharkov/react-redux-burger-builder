59e59848d8e54525d9f12f27d618d28c0e467c69
loki <rus_basket_kg@inbox.ru>
2018-03-18 21:03:08 +0600

    Added redirect to main page and added spinner for order

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 
@@ -51,16 +52,22 @@ class ContactData extends Component {
     }
 
     render() {
-        return(
-            <div className={classes.ContactData}> 
-                <h4>Enter your contact data</h4>
-                <form action="">
+        let form = (
+            <form action="">
                     <input className={classes.Input} type="text" name="name" placeholder="Your name"/>
                     <input className={classes.Input} type="text" name="email" placeholder="Your email"/>
                     <input className={classes.Input} type="text" name="street" placeholder="Street"/>
                     <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner/>;
+        }
+        return(
+            <div className={classes.ContactData}> 
+                <h4>Enter your contact data</h4>
+                {form}
             </div>
         );
     }
