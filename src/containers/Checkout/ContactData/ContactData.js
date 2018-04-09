1f69125dc8675e950432b1ad8f232b85f0f4c566
ruslanzharkov <rus_basket_kg@inbox.ru>
2018-04-09 23:30:00 +0600

    Validity method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,7 +17,11 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'Your Name'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required: true
+                    },
+                    valid: false
                 },
                 street: {
                     elementType: 'input',
@@ -25,7 +29,11 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'Street'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required: true
+                    },
+                    valid: false
                 },
                 zipCode: {
                     elementType: 'input',
@@ -33,7 +41,13 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'ZIP code'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required: true,
+                        minLength: 5,
+                        maxLength: 5
+                    },
+                    valid: false
                 },
                 country: {
                     elementType: 'input',
@@ -41,7 +55,11 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'Country'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required: true
+                    },
+                    valid: false
                 },
                 email: {
                     elementType: 'input',
@@ -49,7 +67,10 @@ class ContactData extends Component {
                         type: 'text',
                         placeholder: 'E-mail'
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required: true
+                    }
                 },
                 deliveryMethod: {
                     elementType: 'select',
@@ -59,7 +80,10 @@ class ContactData extends Component {
                             {value: 'cheapest', displayValue: 'Cheapest'}
                         ]
                     },
-                    value: ''
+                    value: '',
+                    validation: {
+                        required: true
+                    }
                 }
             },
             loading: false
@@ -95,6 +119,23 @@ class ContactData extends Component {
             }); 
     }
 
+    checkValidity(value, rules) {
+        let isValid = false;
+
+        if (rules.required) {
+            isValid = value.trim() !== '';
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength;
+        }
+        return isValid;
+    }
+
     inputChangehandler = (event, inputIdentifier) => {
             const updatedOrderForm = {
                 ...this.state.orderForm
@@ -104,7 +145,9 @@ class ContactData extends Component {
                 ...updatedOrderForm[inputIdentifier]
             };
             updatedFormElement.value = event.target.value;
+            updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
             updatedOrderForm[inputIdentifier] = updatedFormElement;
+            console.log(updatedFormElement.valid);
             this.setState({
                 orderForm: updatedOrderForm
             });
